0d5aad61b093661f8bbf1b8b9fe638c7f60d0491
github username <github email address>
2018-08-07 14:06:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    "This is home page"
+  end
+
+  get '/student' do
+    erb :student
+  end
+
+  post '/student' do
+    erb :student
+  end
 end
